8ddac4a9ef70ac388279cbec5cfcca6a0c65b933
ItzCharityy <113223266+ItzCharityy@users.noreply.github.com>
2022-09-10 05:54:21 +0200

    Update TB.vb

--- GroupProject/GroupProject/TB.vb
+++ GroupProject/GroupProject/TB.vb
@@ -1,16 +1,20 @@
 ﻿Public Class TB
     Inherits Disease
 
+        'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function RateOfTransmission() As Double
         Throw New NotImplementedException()
     End Function
-
+    
+    
+        'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function Mortality() As Double
         Throw New NotImplementedException()
     End Function
 
-
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function GetTypeOfDisease() As String
+            'NOTE THIS IS FOR TESTING AND DEBUGGING
         Return "I am TB"
     End Function
 End Class
