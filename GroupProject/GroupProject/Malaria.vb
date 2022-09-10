c5e394e423a9c75c535984f3124f8a9b658e97f8
ItzCharityy <113223266+ItzCharityy@users.noreply.github.com>
2022-09-10 05:55:48 +0200

    Update Malaria.vb

--- GroupProject/GroupProject/Malaria.vb
+++ GroupProject/GroupProject/Malaria.vb
@@ -1,18 +1,24 @@
 ﻿Public Class Malaria
     Inherits Disease
-
+ 
+    
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function RateOfTransmission() As Double
         Throw New NotImplementedException()
     End Function
 
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function Mortality() As Double
         Throw New NotImplementedException()
     End Function
 
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function GetTypeOfDisease() As String
         Return "I am malaria"
     End Function
 
+    
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Function Caouse() As String
         Return "mosquito"
     End Function
