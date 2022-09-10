54e6b4675c99d77a63f45a10a7e995358b33ea69
ItzCharityy <113223266+ItzCharityy@users.noreply.github.com>
2022-09-10 05:56:59 +0200

    Update Covid-19.vb

--- GroupProject/GroupProject/Covid-19.vb
+++ GroupProject/GroupProject/Covid-19.vb
@@ -1,14 +1,19 @@
 ﻿Public Class Covid_19
     Inherits Disease
 
+    'NOTE THIS IS FOR TESTING AND DEBUGGING    
     Public Overrides Function RateOfTransmission() As Double
         Throw New NotImplementedException()
     End Function
-
+ 
+    
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function Mortality() As Double
         Throw New NotImplementedException()
     End Function
 
+    
+    'NOTE THIS IS FOR TESTING AND DEBUGGING
     Public Overrides Function GetTypeOfDisease() As String
         Return "I am Covid"
     End Function
